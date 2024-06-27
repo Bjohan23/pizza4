4a4e581455fd779a64fd7c4c2b77a50e1a69cea7
Narumi420 <narumicabrerapiscoya@gmail.com>
2024-06-27 17:49:28 -0500

    ya esta los iconos del menu

--- app/Views/inc/sidebar.php
+++ app/Views/inc/sidebar.php
@@ -14,9 +14,11 @@
             <!-- INICIO -->
             <li>
                 <a href="<?php echo APP_URL ?>" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 21H3a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6l5.315-5.316a2 2 0 0 1 2.827 0l5.316 5.316a2 2 0 0 1 0 2.827L15 15m0 0H9m6 0l2.658-2.658a2 2 0 0 1 2.829 0l2.657 2.658a2 2 0 0 1 0 2.829L15 21M9 9L5.342 5.342a2 2 0 0 1 0-2.829L7.658 0a2 2 0 0 1 2.829 0L15 7m0 0v6m0-6l2.658 2.658a2 2 0 0 1 0 2.829L16.342 24a2 2 0 0 1-2.829 0L9 17"></path>
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white">
+                <path fill="currentColor" d="M18.589,0H5.411A5.371,5.371,0,0,0,0,5.318V7.182a1.5,1.5,0,0,0,3,0V5.318A2.369,2.369,0,0,1,5.411,3H18.589A2.369,2.369,0,0,1,21,5.318V18.682A2.369,2.369,0,0,1,18.589,21H5.411A2.369,2.369,0,0,1,3,18.682V16.818a1.5,1.5,0,1,0-3,0v1.864A5.371,5.371,0,0,0,5.411,24H18.589A5.371,5.371,0,0,0,24,18.682V5.318A5.371,5.371,0,0,0,18.589,0Z"/>
+                <path fill="currentColor" d="M3.5,12A1.5,1.5,0,0,0,5,13.5H5l9.975-.027-3.466,3.466a1.5,1.5,0,0,0,2.121,2.122l4.586-4.586a3.5,3.5,0,0,0,0-4.95L13.634,4.939a1.5,1.5,0,1,0-2.121,2.122l3.413,3.412L5,10.5A1.5,1.5,0,0,0,3.5,12Z"/>
+                </svg>
+
                     <span class="ms-3">Inicio</span>
                 </a>
             </li>
@@ -53,9 +55,10 @@
             <!-- Gestión de Usuarios Menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="usuarios-menu" data-collapse-toggle="usuarios-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                    </svg>
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M7,14c-2.21,0-4-1.79-4-4s1.79-4,4-4,4,1.79,4,4-1.79,4-4,4Zm7,10H0v-5c0-1.65,1.35-3,3-3H11c1.65,0,3,1.35,3,3v5ZM21,0H8c-1.65,0-3,1.35-3,3v1.35c.63-.22,1.3-.35,2-.35,2.74,0,5.05,1.84,5.77,4.35l2.23,2.23,3.59-3.59h-2.59v-2h4c1.1,0,2,.9,2,2v4h-2v-2.59l-5,5-2.15-2.15c-.23,1.06-.74,2.01-1.44,2.78,2.23,.18,4.05,1.81,4.49,3.96h8.1V3c0-1.65-1.35-3-3-3Z"/>
+                </svg>
+
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Usuarios</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
@@ -77,9 +80,10 @@
             <!-- gestión de clientes -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="clientes-menu" data-collapse-toggle="clientes-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6s-8 4.5-8 9.5a3.5 3.5 0 0 0 7 0c0-2.5 2-4.5 4.5-4.5s4.5 2 4.5 4.5a3.5 3.5 0 0 0 7 0c0-5-8-9.5-8-9.5z" />
-                    </svg>
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M8,12c-3.309,0-6-2.691-6-6S4.691,0,8,0s6,2.691,6,6-2.691,6-6,6Zm6.744,12c-.211,0-.422-.067-.6-.2-.34-.254-.481-.696-.354-1.101l.941-3.016-2.377-1.934c-.32-.271-.437-.713-.292-1.107.145-.394.519-.655.938-.655h3.001l1.062-2.98c.146-.391.52-.651.937-.651s.791.26.937.651l1.062,2.98h3.001c.42,0,.795.263.939.657s.026.837-.295,1.108l-2.366,1.927.979,2.98c.134.403-.002.847-.339,1.106-.337.259-.801.277-1.156.046l-2.754-1.793-2.708,1.812c-.168.113-.362.169-.556.169Zm-4.807,0H1c-.552,0-1-.448-1-1,0-4.962,4.038-9,9-9h.937c.346,0,.667.18.85.474.182.294.199.662.045.971-.552,1.11-.832,2.307-.832,3.555s.28,2.444.832,3.555c.154.31.137.678-.045.972-.182.294-.504.473-.85.473Z"/>
+                </svg>
+
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Clientes</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
@@ -94,6 +98,7 @@
                     </li>
                 </ul>
             </li>
+
             <!-- Gestión de Roles Menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="roles-menu" data-collapse-toggle="roles-menu">
@@ -117,9 +122,9 @@
             <!-- Gestión de Productos Menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="productos-menu" data-collapse-toggle="productos-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zM9.293 9.293a1 1 0 0 1 1.414 0l.793.793.793-.793a1 1 0 0 1 1.414 1.414l-.793.793.793.793a1 1 0 0 1-1.414 1.414l-.793-.793-.793.793a1 1 0 0 1-1.414-1.414l.793-.793-.793-.793a1 1 0 0 1 0-1.414zM9.293 14.707l.793-.793.793.793a1 1 0 0 1-1.414 1.414l-.793-.793-.793.793a1 1 0 0 1-1.414-1.414l.793-.793-.793-.793a1 1 0 1 1 1.414-1.414l.793.793z" />
-                    </svg>
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M1,19V17A11.01,11.01,0,0,1,11,6.051V4.723a2,2,0,1,1,2,0V6.051A11.01,11.01,0,0,1,23,17v2ZM0,21v2H24V21Z"/>
+                </svg>
 
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Productos</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -138,9 +143,9 @@
             <!-- gestión de pisos Menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="pisos-menu" data-collapse-toggle="pisos-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M2 1a1 1 0 0 0-1 1v16a2 2 0 0 0 2 2h16a1 1 0 0 0 0-2H4V2a1 1 0 0 0-1-1zm10 6v10h4V7h-4zM8 7v10h4V7H8zM4 7h2v10H4V7z" clip-rule="evenodd" />
-                    </svg>
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="m11,22.824L0,17.549v-3.381l11,5.284v3.372Zm2-.002l11-5.312v-3.323l-11,5.267v3.369ZM12,.714L0,6.5v5.536l11,5.274v-3.372L2,9.616v-2.166l10,4.751,10-4.751v2.181l-9,4.309v3.369l11-5.312v-5.497L12,.714Z"/>
+                </svg>
 
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Pisos</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -174,9 +179,11 @@
             <!-- Gestión de Categorías Menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="categorias-menu" data-collapse-toggle="categorias-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1zm8 9a1 1 0 0 0-1 1v7H3V10a1 1 0 0 0-2 0v8a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V10a1 1 0 0 0-1-1zm-5 4a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0v-6zm-6-4a1 1 0 0 0-2 0v10a1 1 0 0 0 2 0V9zm4 4a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0v-6z" clip-rule="evenodd" />
-                    </svg>
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="m24,1v6c0,2.414-1.721,4.434-4,4.899v11.101c0,.552-.448,1-1,1s-1-.448-1-1v-11.101c-2.279-.465-4-2.484-4-4.899V1c0-.552.448-1,1-1s1,.448,1,1v6c0,1.302.839,2.402,2,2.816V1c0-.552.448-1,1-1s1,.448,1,1v8.816c1.161-.414,2-1.514,2-2.816V1c0-.552.448-1,1-1s1,.448,1,1Zm-8,17.918c-1.178.684-2.542,1.082-4,1.082-4.411,0-8-3.589-8-8S7.589,4,12,4V1c0-.349.071-.679.181-.991-.061,0-.12-.009-.181-.009C5.373,0,0,5.373,0,12s5.373,12,12,12c1.416,0,2.768-.258,4.029-.708-.01-.097-.029-.192-.029-.292v-4.082Zm-10-6.918c0,3.309,2.691,6,6,6,1.538,0,2.937-.586,4-1.54v-3.145c-2.361-1.126-4-3.53-4-6.315v-1c-3.309,0-6,2.691-6,6Z"/>
+                </svg>
+
+
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Categorías</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
@@ -195,9 +202,10 @@
             <!-- gentión de sedes menu -->
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="sedes-menu" data-collapse-toggle="sedes-menu">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10-10-4.477-10-10 4.477-10 10-10zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm-1 4h2v6h-2V8z" />
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white">
+                <path fill="currentColor" d="M24,7L21.802,0h-4.802V5h-2V0h-6V5h-2V0H2.198L.024,6.783l-.024,1.217c0,1.005,.385,1.914,1,2.618v10.382c0,1.654,1.346,3,3,3H12c1.654,0,3-1.346,3-3V11.438c.372-.217,.717-.474,1-.795,.733,.832,1.807,1.357,3,1.357h1c.347,0,.678-.058,1-.142v12.142h2V10.618c.615-.703,1-1.612,1-2.618v-1Zm-11,10H3v-5.142c.322,.084,.653,.142,1,.142h1c1.2,0,2.266-.542,3-1.382,.734,.84,1.8,1.382,3,1.382h2v5Z"/>
+                </svg>
+
                     <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Sedes</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
