b1b748e6d733d6b4a3d655e6b39ee5c617c67acf
Bjohan23 <bventurajohanjh@uss.edu.pe>
2024-07-27 19:43:49 -0500

    se agrego el filtrado por fechas y paginacion

--- app/Controllers/VentasController.php
+++ app/Controllers/VentasController.php
@@ -13,7 +13,27 @@ class VentasController extends Controller
         $rolUsuario = $usuarioModel->getRolesUsuarioAutenticado(Session::get('usuario_id'));
 
         $ventaModel = $this->model('ComprobanteVenta');
-        $ventas = $ventaModel->getVentas();
-        $this->view('ventas/index', ['ventas' => $ventas, 'rolUsuario' => $rolUsuario]);
+
+        $fecha = isset($_POST['fecha']) ? $_POST['fecha'] : null;
+        $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+        $limite = 10; // Número de ventas por página
+
+        if ($fecha) {
+            $ventas = $ventaModel->getVentasPorFecha($fecha, $pagina, $limite);
+            $totalVentas = $ventaModel->getTotalVentasPorFecha($fecha);
+        } else {
+            $ventas = $ventaModel->getVentas($pagina, $limite);
+            $totalVentas = $ventaModel->getTotalVentas();
+        }
+
+        $totalPaginas = ceil($totalVentas / $limite);
+
+        $this->view('ventas/index', [
+            'ventas' => $ventas,
+            'rolUsuario' => $rolUsuario,
+            'pagina' => $pagina,
+            'totalPaginas' => $totalPaginas,
+            'fecha' => $fecha
+        ]);
     }
 }
